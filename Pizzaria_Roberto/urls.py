dd89504d1fcc8ebd0d3f58a056346ad5cb57eb36
JEduardo <j3ffsil4.890@gmail.com>
2025-07-23 15:56:52 -0300

    Pag login

--- Pizzaria_Roberto/urls.py
+++ Pizzaria_Roberto/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import index
-
+from core.views import index, login
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('login/', login, name='login')
 ]
