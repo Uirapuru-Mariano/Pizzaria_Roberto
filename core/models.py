677d8c400ee2bde436667babc0947581a81a827a
Mariano <uirapurumariano01@gmail.com>
2025-10-22 21:01:36 -0300

    models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
 # Create your models here.
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    categoria = models.CharField(max_length=50)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.nome
+
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=20)
+    senha = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+
+class Endereco(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    logradouro = models.CharField(max_length=150)
+    numero_da_casa = models.CharField(max_length=10)
+    complemento = models.CharField(max_length=100, blank=True, null=True)
+    bairro = models.CharField(max_length=100)
+    cep = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.logradouro}, {self.numero_da_casa} - {self.bairro}"
+
+
+class Funcionario(models.Model):
+    nome = models.CharField(max_length=100)
+    cargo = models.CharField(max_length=50)
+    telefone = models.CharField(max_length=20)
+    email = models.EmailField()
+    senha = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.nome} ({self.cargo})"
+
+
+class Pedido(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    atendente = models.ForeignKey(Funcionario, on_delete=models.SET_NULL, null=True)
+    data_pedido = models.DateField()
+    valor_total = models.DecimalField(max_digits=10, decimal_places=2)
+    forma_pagamento = models.CharField(max_length=20)
+    taxa_entrega = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f"Pedido #{self.id} - Cliente: {self.cliente.nome}"
+
+
+class ItemPedido(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    quantidade = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.quantidade}x {self.produto.nome} (Pedido {self.pedido.id})"
